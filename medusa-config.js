c85609bb7fe760c6192a8a076bb5670e22cac9ba
Haiko Erinkveld <haiko.erinkveld@gmail.com>
2023-06-08 22:04:27 +0200

    initial commit

--- medusa-config.js
+++ medusa-config.js
@@ -63,18 +63,18 @@ const plugins = [
 ];
 
 const modules = {
-  /*eventBus: {
-    resolve: "@medusajs/event-bus-redis",
+  eventBus: {
+    resolve: '@medusajs/event-bus-redis',
     options: {
-      redisUrl: REDIS_URL
-    }
+      redisUrl: REDIS_URL,
+    },
   },
   cacheService: {
-    resolve: "@medusajs/cache-redis",
+    resolve: '@medusajs/cache-redis',
     options: {
-      redisUrl: REDIS_URL
-    }
-  },*/
+      redisUrl: REDIS_URL,
+    },
+  },
 };
 
 /** @type {import('@medusajs/medusa').ConfigModule["projectConfig"]} */
@@ -85,7 +85,7 @@ const projectConfig = {
   database_url: DATABASE_URL,
   admin_cors: ADMIN_CORS,
   // Uncomment the following lines to enable REDIS
-  // redis_url: REDIS_URL
+  redis_url: REDIS_URL,
 };
 
 /** @type {import('@medusajs/medusa').ConfigModule} */
